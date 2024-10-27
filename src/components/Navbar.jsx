3e5ad9c404b3ce0baf53d1d72252f6ffdb640ace
Jawwad Junaid <jawwadjunaid17@gmail.com>
2024-10-27 18:52:28 +0500

    logo file updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 import {styles} from '../style';
 import { navLinks } from '../constants';
-import {logo, menu, close} from '../assets';
+import { menu, close} from '../assets';
+import {logo} from '../assets';
 
 const Navbar = () => {
     const [active, setActive] = useState(`'`); 
