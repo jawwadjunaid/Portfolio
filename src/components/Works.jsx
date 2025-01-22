b36a3c20993846da3744108c25c91a4b8bc125e0
Jawwad Junaid <jawwadjunaid17@gmail.com>
2025-01-22 18:54:09 +0500

    update index.html

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -61,7 +61,7 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
+        <p className={`${styles.sectionSubText}`}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
@@ -70,11 +70,10 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Following projects showcase my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos. It reflects my ability to
-          solve complex problems, work with different technologies, and manage
-          projects effectively.
+          Following projects showcase my skills and experience through real-world
+          examples of my work. Each project is briefly described with links to code
+          repositories and live demos. It reflects my ability to solve complex problems,
+          work with different technologies, and manage projects effectively.
         </motion.p>
       </div>
 
@@ -88,3 +87,4 @@ const Works = () => {
 };
 
 export default SectionWrapper(Works, "");
+
